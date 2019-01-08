92037d2788d8020307a9cebc8fe209ac1eb25896
friend <friend@friends-iMac.local>
2019-01-08 11:36:56 +0800

    my vuepro first

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Posts from '@/components/Posts'
+import NewPost from '@/components/NewPost'
+import EditPost from '@/components/EditPost'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: '/posts'
+    },
+    {
+      path: '/posts',
+      name: 'Posts',
+      component: Posts
+    },
+    {
+      path: '/posts/new',
+      name: 'NewPost',
+      component: NewPost
+    },
+    {
+      path: '/posts/:id',
+      name: 'EditPost',
+      component: EditPost
+    }
+  ]
+})
